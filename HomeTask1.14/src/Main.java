c0428c0841c6c85a82a065015468d547b53748d2
Deeerwy <125497941+Deeerwy@users.noreply.github.com>
2023-04-05 18:32:31 +0300

    Add files via upload

---
+++ HomeTask1.14/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> numList = new ArrayList<>();
+
+        Runnable genNumbers = () -> {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++) {
+                numList.add(rand.nextInt(1000));
+            }
+        };
+
+        Runnable sortNumbers = () -> {
+            Collections.sort(numList);
+            System.out.println("Sorted numbers: " + numList);
+        };
+
+        Thread t1 = new Thread(genNumbers);
+        t1.start();
+        t1.join();
+
+        Thread t2 = new Thread(sortNumbers);
+        t2.start();
+    }
+
+}
